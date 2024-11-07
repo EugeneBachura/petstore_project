0c3e5c913a3c266bc149b07d1e664a47f6950d3f
Eugene Bachura <eugenebachura@gmail.com>
2024-11-07 21:09:24 +0100

    Dodany testy PHPUnit dla operacji CRUD

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/pets');
 
         $response->assertStatus(200);
     }
